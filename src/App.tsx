af4440985aaab1d11886077679b7ceae275660e0
BrunoVigel <brunorosav@gmail.com>
2022-09-01 20:40:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import {champs} from './champs'
+import Kalec from './assets/kalec-800x525.png'
+
+function App() {
+  const [number, setNumber] = useState(0)
+
+  function randomizer() {
+    setNumber(Math.floor(Math.random() * (25 - 1 + 1) + 1))
+  }
+
+  function reset() {
+    setNumber(0)
+  }
+
+  return (
+    <div className="App">
+      <div className='home'>
+        <h1>League of Legends ADCarry Randomizer</h1>
+        <img src={Kalec} alt="Kalec" />
+        <div>
+          <button onClick={randomizer}>Iniciar</button>
+          <button onClick={reset}>Resetar</button>
+        </div>
+      </div>
+      {
+        Object.entries(champs).map(([key, value]) => {
+          if (number === value.id)  {
+            return(
+              <div className='result'>
+                <img key={value.id} src={value.img} alt="" />
+                <p>{value.name}</p>
+                <div className='info'>
+                  <div>
+                    <p>Core runes</p>
+                    <img src={value.rune} alt="" />
+                  </div>
+                  <div>
+                    <p>Core items</p>
+                    <img src={value.build} alt="" />
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        })
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/champs.ts
@@ -0,0 +1,177 @@
+export const champs = [
+    {
+        id: 1,
+        name: 'Aphelios',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aphelios_0.jpg',
+        rune: 'src/assets/runes/13111-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 2,
+        name: 'Ashe',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ashe_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 3,
+        name: 'Caitlyn',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+        rune: 'src/assets/runes/13111-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 4,
+        name: 'Draven',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Draven_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 5,
+        name: 'Ezreal',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ezreal_0.jpg',
+        rune: 'src/assets/runes/11211-4230.png',
+        build: 'src/assets/builds/ezreal.png'
+    },
+    {
+        id: 6,
+        name: 'Jhin',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg',
+        rune: 'src/assets/runes/13111-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 7,
+        name: 'Jinx',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 8,
+        name: "kai'sa",
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kaisa_0.jpg',
+        rune: 'src/assets/runes/24214-1011.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 9,
+        name: 'Kalista',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kalista_0.jpg',
+        rune: 'src/assets/runes/24214-1011.png',
+        build: 'src/assets/builds/kalista.png'
+    },
+    {
+        id: 10,
+        name: "Kog'Maw",
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/KogMaw_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kalista.png'
+    },
+    {
+        id: 11,
+        name: 'Lucian',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lucian_0.jpg',
+        rune: 'src/assets/runes/11211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 12,
+        name: 'Miss Fortune',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/MissFortune_0.jpg',
+        rune: 'src/assets/runes/13111-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 13,
+        name: 'Nilah',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nilah_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/nilah.png'
+    },
+    {
+        id: 14,
+        name: 'Samira',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Samira_0.jpg',
+        rune: 'src/assets/runes/14211-4230.png',
+        build: 'src/assets/builds/samira.png'
+    },
+    {
+        id: 15,
+        name: 'Sivir',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Sivir_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 16,
+        name: 'Tristana',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Tristana_0.jpg',
+        rune: 'src/assets/runes/24214-1011.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 17,
+        name: 'Twitch',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Twitch_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 18,
+        name: 'Varus',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Varus_0.jpg',
+        rune: 'src/assets/runes/24214-1011.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 19,
+        name: 'Vayne',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Vayne_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/vayne.png'
+    },
+    {
+        id: 20,
+        name: 'Xayah',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Xayah_0.jpg',
+        rune: 'src/assets/runes/12211-4230.png',
+        build: 'src/assets/builds/kraken-vendaval+crit.png'
+    },
+    {
+        id: 21,
+        name: 'Zeri',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zeri_0.jpg',
+        rune: 'src/assets/runes/12211-3012.png',
+        build: 'src/assets/builds/zeri.png'
+    },
+    {
+        id: 22,
+        name: 'Ziggs',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ziggs_0.jpg',
+        rune: 'src/assets/runes/32211-1301.png',
+        build: 'src/assets/builds/mage1.png'
+    },
+    {
+        id: 23,
+        name: 'Heimerdinger',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Heimerdinger_0.jpg',
+        rune: 'src/assets/runes/32211-1301.png',
+        build: 'src/assets/builds/mage1.png'
+    },
+    {
+        id: 24,
+        name: 'Syndra',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Syndra_0.jpg',
+        rune: 'src/assets/runes/32211-1301.png',
+        build: 'src/assets/builds/mage2.png'
+    },
+    {
+        id: 25,
+        name: 'Senna',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Senna_0.jpg',
+        rune: 'src/assets/runes/13111-4230.png',
+        build: 'src/assets/builds/senna.png'
+    },
+]
